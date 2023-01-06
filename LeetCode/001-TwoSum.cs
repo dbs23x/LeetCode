bead38ebd9edbf6302a7d7263afb0382902d7c36
dbs23x <dbs23x@gmail.com>
2023-01-05 23:38:56 -0300

    Add the C# solution

---
+++ LeetCode/001-TwoSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _001_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    return new int[] { dic[nums[i]], i };
+                else
+                    dic[target - nums[i]] = i;
+            }
+
+            return new int[] { };
+        }
+    }
+}
